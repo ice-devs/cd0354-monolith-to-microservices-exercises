042a2055d55a34714ebdf7222f30ca9dbf52f1b0
ice-devs <othukeotubu@gmail.com>
2022-09-12 17:26:31 +0100

    new commit

--- lesson-4-automating-development/exercises/travis_env_var_exercise/server.js
+++ lesson-4-automating-development/exercises/travis_env_var_exercise/server.js
@@ -1,11 +1,11 @@
-
+const name = process.env.NAME;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log("${name} rock!");
     await sleep(5000);
   }
 }
